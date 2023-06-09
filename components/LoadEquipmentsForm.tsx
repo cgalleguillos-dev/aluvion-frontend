eb9fc4632c7253b346ba0727c1b7f6c1c421e323
Shpm21 <galleguilloscristian2@gmail.com>
2023-06-09 18:27:04 -0400

    feat: add equipments to json

--- components/LoadEquipmentsForm.tsx
+++ components/LoadEquipmentsForm.tsx
@@ -11,7 +11,7 @@ const LoadEquipmentsForm: React.FC = () => {
     <div className="">
       <div>
         <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Upload file</label>
-        <input className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="file_input" type="file" accept=".txt" onChange={handleFileChange} />
+        <input className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="file_input" type="file" accept=".json" onChange={handleFileChange} />
       </div>
       <ShowEquipmentsTable equipments={equipments} />
       <button
--- hooks/useEquipmentLoader.ts
+++ hooks/useEquipmentLoader.ts
@@ -29,58 +29,18 @@ const useEquipmentLoader = () => {
 
   const saveEquipments = async () => {
     const URL = process.env.NEXT_PUBLIC_API + '/base-equipment/bulk';
-    console.log(equipments);
     const response = await axios.post(URL, equipments);
-    console.log(response);
   };
 
   useEffect(() => {
     const loadEquipments = () => {
       if (file) {
         const reader = new FileReader();
-        const loadedEquipments: BaseEquipment[] = [];
-        let baseEquipment: BaseEquipment | null = null;
-        let currentComponent: Component | null = null;
 
-        reader.onload = function (e) {
-          const contents = e.target?.result;
-          const lines = contents?.toString().split('\n');
-
-          lines?.forEach(function (line: string) {
-            const trimmedLine = line.trim();
-
-            if (trimmedLine.length > 0) {
-              const [key, value] = trimmedLine.split(':').map(part => part.trim());
-
-              if (key === 'description') {
-                baseEquipment = { description: value, components: [] };
-              } else if (key === 'components') {
-                baseEquipment!.components = [];
-              } else if (key === '- description') {
-                currentComponent = { description: value, pins: [] };
-                baseEquipment!.components.push(currentComponent);
-              } else if (key === '- comunicationType') {
-                currentComponent!.pins.push({
-                  comunicationType: value,
-                  signalType: '',
-                  pinNumber: 0
-                });
-              } else if (key === 'signalType') {
-                const lastPin = currentComponent!.pins[currentComponent!.pins.length - 1];
-                lastPin.signalType = value;
-              } else if (key === 'pinNumber') {
-                const lastPin = currentComponent!.pins[currentComponent!.pins.length - 1];
-                lastPin.pinNumber = parseInt(value);
-              }
-            } else if (trimmedLine.length === 0 && baseEquipment) {
-              loadedEquipments.push(baseEquipment);
-              baseEquipment = null;
-            }
-          });
-
-          setEquipments(loadedEquipments);
-        };
-
+        reader.onload = (event: ProgressEvent<FileReader>) => {
+          const equipments = JSON.parse(event.target?.result as string);
+          setEquipments(equipments);
+        }
         reader.readAsText(file);
       }
     };

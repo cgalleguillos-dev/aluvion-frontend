45f7292d49f80b4ebf7cfa81e70aff064b3cb252
cgalleguillos-dev <galleguilloscristian2@gmail.com>
2023-06-10 23:42:54 +0200

    feat: add id to interfaces

--- config/interfaces.ts
+++ config/interfaces.ts
@@ -1,14 +1,17 @@
 export interface BaseEquipment {
+  id: string;
   description: string;
   components: Component[];
 }
 
 export interface Component {
+  id: string;
   description: string;
   pins: Pin[];
 }
 
 export interface Pin {
+  id: string;
   comunicationType: string;
   signalType: string;
   pinNumber: number;

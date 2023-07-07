163c534f9aa88082bc79e24341a405b29de3e093
cgalleguillos-dev <galleguilloscristian2@gmail.com>
2023-07-07 03:48:16 -0400

    feat: create login page

---
+++ app/auth/login/page.tsx
@@ -0,0 +1,12 @@
+import { LoginForm } from '@/components';
+import { NextPage } from 'next';
+
+const Login: NextPage = () => {
+  return (
+    <>
+      <LoginForm />
+    </>
+  )
+}
+
+export default Login;

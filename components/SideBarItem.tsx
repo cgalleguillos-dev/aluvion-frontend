5b1ebef0d9ecf1c0b2fb79b3d8b08cbcf4f2b185
cgalleguillos-dev <galleguilloscristian2@gmail.com>
2023-07-20 16:37:03 -0400

    fix: fix the sidebar

--- components/SideBarItem.tsx
+++ components/SideBarItem.tsx
@@ -4,13 +4,18 @@ import React from 'react'
 
 interface Props {
   iSidebar: ISideBar;
+  isActive: boolean;
+  onItemClick: () => void;
 }
 
-export const SideBarItem: React.FC<Props> = ({ iSidebar: { href, label, subLabel }
-}) => {
+export const SideBarItem: React.FC<Props> = ({ iSidebar: { href, label, subLabel }, isActive, onItemClick }) => {
   return (
     <>
-      <Link href={href} className="inline-flex items-center w-full px-2 py-3 space-x-2 transition duration-150 ease-linear border-b border-slate-700 hover:bg-white/5">
+      <Link href={href} className={`inline-flex items-center w-full px-2 py-3 space-x-2 transition duration-150 ease-linear border-b border-slate-700 hover:bg-white/5
+        ${isActive ? 'bg-white/5' : ''
+        }`}
+        onClick={onItemClick}
+      >
         <div className="flex flex-col">
           <span className="text-lg font-bold leading-5 text-white">{label}</span>
           <span className="hidden text-sm text-white/50 md:block">{subLabel}</span>
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,13 +1,20 @@
+'use client'
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
 import { SIDE_BAR } from '@/constants'
 import { SideBarItem } from './SideBarItem'
 
 export const SideBar: React.FC = () => {
+  const [activeIndex, setActiveIndex] = useState<number>();
+
+  const handleItemClick = (index: number) => {
+    setActiveIndex(index);
+  }
+
   return (
     <div className="w-64 h-screen bg-slate-100 text-slate-300 selection:bg-blue-600 selection:text-white">
       <div className="flex flex-col">
-        <div id="menu" className="z-10 w-64 h-screen min-h-screen bg-gray-900 text-slate-300 ">
+        <div id="menu" className="z-10 w-64 h-screen min-h-screen bg-[#0e314f] text-slate-300 ">
           <div id="logo" className="px-6 my-4">
             <h1 className="text-lg font-bold text-white md:text-2xl">Inicio<span className="text-blue-500"></span></h1>
 
@@ -26,10 +33,15 @@ export const SideBar: React.FC = () => {
           <div id="nav" className="w-full px-6">
             {
               SIDE_BAR.map((iSidebar, index) => (
-                <SideBarItem key={index} iSidebar={iSidebar} />
+                <SideBarItem key={index} iSidebar={iSidebar}
+                  isActive={activeIndex === index}
+                  onItemClick={() => handleItemClick(index)}
+                />
               ))
             }
-            <Link href="/homes/logout" className="inline-flex items-center w-full px-2 py-3 space-x-2 transition duration-150 ease-linear hover:bg-white/5">
+            <Link href="/auth/login" className="inline-flex items-center w-full px-2 py-3 space-x-2 transition duration-150 ease-linear hover:bg-white/5"
+              onClick={() => localStorage.removeItem('token')}
+            >
               <div className="flex flex-col">
                 <span className="text-lg font-bold leading-5 text-slate-300">Salir</span>
               </div>

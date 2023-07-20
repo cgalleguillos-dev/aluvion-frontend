7f6dbb7499a7b612f3f2bb57963892e6e0bc24e9
cgalleguillos-dev <galleguilloscristian2@gmail.com>
2023-07-20 16:35:28 -0400

    feat: add loading component to form

--- components/LoadEquipmentsForm.tsx
+++ components/LoadEquipmentsForm.tsx
@@ -2,6 +2,7 @@
 import { CustomButton, ShowEquipmentsTable } from "./common";
 import useEquipmentLoader from "@/hooks/useEquipmentLoader";
 import Loading from "./common/Loading";
+import { BaseNavbar } from "./BaseNavbar";
 
 
 const LoadEquipmentsForm: React.FC = () => {
@@ -9,20 +10,22 @@ const LoadEquipmentsForm: React.FC = () => {
 
 
   return (
-    <div className="w-screen h-screen mx-10 mt-4">
-      <div className="sm:-mx-6 lg:-mx-10">
-        <div className="inline-block w-full py-2 align-middle sm:px-6 lg:px-8">
-          <div>
-            <input className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="file_input" type="file" accept=".json" onChange={handleFileChange} />
-          </div>
-          <CustomButton
-            description="Cargar"
-            onClick={saveEquipments}
-          />
-          <ShowEquipmentsTable equipments={equipments} />
+    <div className="w-screen h-screen">
+      {
+        loading && <Loading />
+      }
+      <BaseNavbar title="Cargar Equipos" isPage >
+        <div>
+          <input className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="file_input" type="file" accept=".json" onChange={handleFileChange} />
         </div>
-        {loading && <Loading />}
-      </div>
+        <CustomButton
+          description="Cargar"
+          onClick={saveEquipments}
+          enabled={equipments.length > 0}
+        />
+      </BaseNavbar>
+      <ShowEquipmentsTable equipments={equipments} />
+      {loading && <Loading />}
     </div>
   )
 }

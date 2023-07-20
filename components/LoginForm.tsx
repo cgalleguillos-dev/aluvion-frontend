8eb90945a3c4293be0ffcaecefcd403c1bf8b932
cgalleguillos-dev <galleguilloscristian2@gmail.com>
2023-07-20 16:35:40 -0400

    feat: implement login form

--- components/LoginForm.tsx
+++ components/LoginForm.tsx
@@ -1,17 +1,52 @@
-import React from 'react';
-
+'use client';
+import React, { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import axios from 'axios';
 interface Props {
 
 }
 
+type LoginFormData = {
+  email: string,
+  password: string
+};
+
 export const LoginForm: React.FC<Props> = ({ }) => {
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string>('');
+  const router = useRouter();
+  const { register, handleSubmit, watch, formState: { errors } } = useForm<LoginFormData>();
+  const handleLogin: SubmitHandler<LoginFormData> = async data => {
+    try {
+      setLoading(true);
+      const response = await axios.post('http://localhost:3005/auth/login', {
+        email: data.email,
+        password: data.password
+      });
+      console.log(response);
+      if (response) {
+        localStorage.setItem('token', response.data.token);
+        router.push('/auth/base-equipments');
+      }
+
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+      setError('Credenciales inválidas');
+      setTimeout(() => {
+        setError('');
+      }
+        , 3000);
+    }
+  }
   return (
     <form
-      // onSubmit={handleSubmit(handleLogin)}
+      onSubmit={handleSubmit(handleLogin)}
       className="flex flex-col justify-center flex-1 min-h-full px-6 py-12 lg:px-8">
 
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-        <h2 className="mt-10 text-2xl font-bold leading-9 tracking-tight text-center text-black">
+        <h2 className="mt-10 text-2xl leading-9 tracking-tight text-center text-black">
           Proyecto Aluvión
         </h2>
       </div>
@@ -23,10 +58,10 @@ export const LoginForm: React.FC<Props> = ({ }) => {
           </label>
           <div className="mt-2">
             <input
-              // {...register("email")}
+              {...register("email")}
               required
               autoComplete="email"
-              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6"
+              className="block w-full rounded-md border-0 py-1.5 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6"
             />
           </div>
         </div>
@@ -36,18 +71,14 @@ export const LoginForm: React.FC<Props> = ({ }) => {
             <label htmlFor="password" className="block text-sm font-medium leading-6 text-black">
               Contraseña
             </label>
-            <div className="text-sm">
-              <a href="#" className="font-semibold text-gray-900 hover:text-gray-700 ">
-                Forgot password?
-              </a>
-            </div>
           </div>
           <div className="mt-2">
             <input
-              // {...register("password")}
+              {...register("password")}
               required
               autoComplete="current-password"
-              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6"
+              type="password"
+              className="block w-full rounded-md border-0 py-1.5 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6"
             />
           </div>
         </div>
@@ -55,22 +86,22 @@ export const LoginForm: React.FC<Props> = ({ }) => {
         <div>
           <button
             type="submit"
-            className="mt-5 flex w-full justify-center rounded-mdtext-white bg-gray-900 rounded hover:bg-gray-700 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm  focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            className="mt-5 flex w-full justify-center rounded-mdtext-white bg-[#db6e59] rounded hover:bg-gray-700 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm  focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
-            {/* {errors.password && <span>This field is required</span>}
-          {errors.email && <span>This field is required</span>} */}
-            Sign in
+            {errors.password && <span>Este campo es requerido</span>}
+            {errors.email && <span>Este campo es requerido</span>}
+            Acceder
           </button>
         </div>
-        {/* {
-        error && (
-          <div className="mt-5 flex w-full justify-center p-2
+        {
+          error && (
+            <div className="mt-5 flex w-full justify-center p-2
             bg-red-500 bg-opacity-50 rounded-md px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm 
           ">
-            {error}
-          </div>
-        )
-      } */}
+              {error}
+            </div>
+          )
+        }
 
 
       </div>
